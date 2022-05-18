88ef05bb60766db733343f7f335e0ffda18dbb61
Tamagochi02 <mirytamara@gmail.com>
2022-05-18 08:56:03 -0500

    name changed

--- controllers/ruta.js
+++
@@ -1,8 +0,0 @@
-import { prisma } from "../../lib/prisma";
-app.get('/feed', async (req, res) => {
-    const posts = await prisma.post.findMany({
-      where: { published: true },
-      include: { author: true },
-    })
-    res.json(posts)
-  })
