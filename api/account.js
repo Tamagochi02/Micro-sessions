ed6eb0ae049e9145335abd278798738cd926c7a4
Tamagochi02 <mirytamara@gmail.com>
2022-05-18 08:55:36 -0500

    api

---
+++ api/account.js
@@ -0,0 +1,20 @@
+import { get, post, put } from "../controllers/accountController"; 
+
+function account(req, res) {
+    switch (req.method) {
+        case 'GET':
+            get(req, res)
+            break;
+        case 'POST':
+            post(req, res)
+            break;
+
+        case 'PUT':
+            put(req, res)
+            break;
+
+        default:
+            res.status(400).json({})
+            break;
+    }
+}
